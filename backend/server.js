d1fb8e323e714e337dd03a53c301f67c42921d22
khushi <khushitanwer227@gmail.com>
2026-07-23 17:10:43 +0530

    Update server.js
    Removed duplicate auth route and adjusted task route.

--- backend/server.js
+++ backend/server.js
@@ -24,10 +24,8 @@ mongoose.connect(process.env.DATABASE_URI || 'mongodb://127.0.0.1:27017/taskcraf
 const authRoutes = require('./routes/authRoutes');
 const taskRoutes = require('./routes/taskRoutes');
 
-app.use('/api/auth', authRoutes);
-app.use('/auth', authRoutes);
 
-app.use('/api/tasks', taskRoutes);
+app.use('/auth', authRoutes);
 app.use('/tasks', taskRoutes);
 
 app.get('/', (req, res) => {
